6ba302852ea475c44d589ab75ff810890af234b7
mandraitsara <pmandraitsara@gmail.com>
2024-05-27 14:16:09 +0300

    ajout dernier

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,18 @@
-const toto = document.querySelector('.toto');
-console.log("hellonopm");
+const newProjectBox = document.querySelector('.new-project');
+        const gearIcon = document.querySelector('.gear-icon');
+        const box = document.querySelector('.delete-box')
+
+        newProjectBox.addEventListener('mouseenter', function() {
+            gearIcon.classList.add('show-gear')
+        })
+        
+        newProjectBox.addEventListener('mouseleave', function() {
+            gearIcon.classList.remove('show-gear')
+        })
+       gearIcon.addEventListener('click', function(){
+            if(box.style.display === "none") {
+                box.style.display = "block";
+            } else {
+                box.style.display = "none";
+            }
+})
